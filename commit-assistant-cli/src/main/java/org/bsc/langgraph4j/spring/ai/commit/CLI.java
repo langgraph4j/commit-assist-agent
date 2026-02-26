725febb7c0365daa8a56f4c2a888bbd9774274c7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-02-26 12:47:00 +0100

    style(CLI.java): remove unused imports

--- commit-assistant-cli/src/main/java/org/bsc/langgraph4j/spring/ai/commit/CLI.java
+++ commit-assistant-cli/src/main/java/org/bsc/langgraph4j/spring/ai/commit/CLI.java
@@ -1,16 +1,10 @@
 package org.bsc.langgraph4j.spring.ai.commit;
 
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.SimpleTheme;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.WaitingDialog;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.input.MouseAction;
-import com.googlecode.lanterna.input.MouseActionType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.MouseCaptureMode;
